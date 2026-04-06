1a3f63aeb6c2a7674c8a3c927230a784256a894f
mselle14 <mselle14@aum.edu>
2026-04-06 15:07:07 -0500

    Updated greeting message.

--- Marshon_Lab1one/Marshon_Lab1one.cpp
+++ Marshon_Lab1one/Marshon_Lab1one.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello from Marshon! This is my first Git commit.";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
