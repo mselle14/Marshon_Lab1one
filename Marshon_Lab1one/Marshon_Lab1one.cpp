571860ffc0b551fdeae935e4c1399d46d2022d8c
mselle14 <mselle14@aum.edu>
2026-04-06 15:15:48 -0500

    Added second output line

--- Marshon_Lab1one/Marshon_Lab1one.cpp
+++ Marshon_Lab1one/Marshon_Lab1one.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main()
 {
     cout << "Hello from Marshon! This is my first Git commit.";
+    cout << "Git makes collaboration easy!" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
