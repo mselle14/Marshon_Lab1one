4ca72f765ce2387d373cc134fb398c731db4fb07
dwhite35aum <dwhite35@aum.edu>
2026-04-06 15:59:22 -0500

    Added output line by Dontrell White

--- Marshon_Lab1one/Marshon_Lab1one.cpp
+++ Marshon_Lab1one/Marshon_Lab1one.cpp
@@ -8,6 +8,7 @@ int main()
 {
     cout << "Hello from Marshon! This is my first Git commit.";
     cout << "Git makes collaboration easy!" << endl;
+    cout << "Changes made by [Student B Name]!" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
