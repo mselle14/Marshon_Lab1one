d3178399255c7a0ea20fe05882cdd444f52f07ba
mselle14 <mselle14@aum.edu>
2026-04-08 14:51:01 -0500

    Add greeting feauture on feature branch

--- Marshon_Lab1one/Marshon_Lab1one.cpp
+++ Marshon_Lab1one/Marshon_Lab1one.cpp
@@ -11,6 +11,7 @@ int main()
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Dontrell!" << endl;
     cout << "Lab 1 completed on April 6 2026!" << endl;
+    cout << "Feature by Marshon : Branching is awesome!" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## About This Project
 This is a simple C++ console application created for Git Lab 1.
 
+## Changes Made By Marshon
+Added a feature greeting
+
 ## What I learned
 - How to create a Git repository in Visual Studio
 - How to commit and push changes
